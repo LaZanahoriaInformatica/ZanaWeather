ae5b77bb542b288eaf8dcaf4712ad829b1ad9d5f
duckiermango <miguelangelarocas@gmail.com>
2018-01-15 13:46:27 +0100

    Prueba de cambio-migue

--- app/src/main/java/com/kalderius/agus/zanaweather/MainActivity.java
+++ app/src/main/java/com/kalderius/agus/zanaweather/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"Prueba", Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"Prueba", Toast.LENGTH_SHORT).show();
     }
 }
