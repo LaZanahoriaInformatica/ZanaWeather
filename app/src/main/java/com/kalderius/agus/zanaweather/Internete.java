7143f2c58dd8f7d81ef59398ee20a125340bedb4
Agneka <35424290+Agneka@users.noreply.github.com>
2018-01-15 20:53:38 +0100

    Funcionalidad de parser completa. He terminado

--- app/src/main/java/com/kalderius/agus/zanaweather/Internete.java
+++ app/src/main/java/com/kalderius/agus/zanaweather/Internete.java
@@ -2,6 +2,9 @@ package com.kalderius.agus.zanaweather;
 
 import android.os.AsyncTask;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class Internete extends AsyncTask {
     private String id;
@@ -18,13 +21,34 @@ public class Internete extends AsyncTask {
     public Internete(String id){
         super();
         this.id = id;
+
     }
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Tiempo getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(Tiempo tiempo) {
+        this.tiempo = tiempo;
+    }
 
     @Override
     protected Object doInBackground(Object[] objects) {
 
+        try {
+            ParseadorSAX parseador = new ParseadorSAX(new URL("http://www.aemet.es/xml/municipios/localidad_"+this.id+".xml"));
+            parseador.parse();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
---
+++ app/src/main/java/com/kalderius/agus/zanaweather/ParseadorSAX.java
@@ -0,0 +1,56 @@
+package com.kalderius.agus.zanaweather;
+
+import android.net.ParseException;
+
+import org.xml.sax.InputSource;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+/**
+ * Created by Agus on 15/01/2018.
+ */
+
+public class ParseadorSAX {
+    private InputSource url;
+
+
+    public ParseadorSAX(URL url) {
+        InputSource is = null;
+        try {
+            is = new InputSource(url.openStream());
+            is.setEncoding("ISO-8859-1");
+            this.url = is;
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+
+    public void parse()
+    {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+
+        try
+        {
+            SAXParser parser = factory.newSAXParser();
+            ManejadorSax manejador = new ManejadorSax();
+            parser.parse(this.url, manejador);
+        } catch(ParseException ex){
+            ex.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
