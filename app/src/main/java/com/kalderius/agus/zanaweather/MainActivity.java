3a3e863e151ad22914558206fbcaa9416dc2128b
Agneka <35424290+Agneka@users.noreply.github.com>
2018-01-15 13:55:47 +0100

    Version 0.1 Agus

--- app/src/main/java/com/kalderius/agus/zanaweather/MainActivity.java
+++ app/src/main/java/com/kalderius/agus/zanaweather/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"Prueba", Toast.LENGTH_LONG).show();
         Toast.makeText(this,"Prueba", Toast.LENGTH_LONG).show();
+        System.out.println("HolaMundo");
     }
 }
